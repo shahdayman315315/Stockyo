f55f8e81ef012b171e81f5866d84d2fd48ba7eb8
shahdayman315315 <shahdayman315315@gmail.com>
2026-02-06 22:13:46 +0200

    add database settings

--- Backend/StockyoProject/Stockyo.Infrastructure/Data/AppDbContext.cs
+++ Backend/StockyoProject/Stockyo.Infrastructure/Data/AppDbContext.cs
@@ -11,8 +11,9 @@ namespace Stockyo.Infrastructure.Data
 {
     public class AppDbContext:IdentityDbContext<ApplicationUser>
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-       
+
         public DbSet<Store> Stores { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
